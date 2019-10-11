a7611d786148aa55945b629193629bb08862ae85
dman444 <davem@DMATHISS10.ftsc.com>
2019-10-11 08:10:57 -0500

    Initial upload.

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
-
+locals {
+    location_codes = {
+        eastus          = "ea"
+        eastus2         = "e2"
+        southcentraus   = "sc"
+        westus          = "we"
+        westus2         = "w2"
+    }
+}
